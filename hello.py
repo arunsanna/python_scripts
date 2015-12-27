c4d69a5fdf144630db49ca5722d740442569b8d8
Arun Sanna <arunsanna@arun.reancloud.com>
2015-12-26 20:29:39 -0500

    intial commit after testing the I/0 of python

---
+++ hello.py
@@ -0,0 +1,11 @@
+'''This the intial program designed by arunsanna to check the input and output
+   the program in the program sys.argv will read the inputs and 
+   [0]--> refers to the frist argument passed
+   [1]--> refers to the second argument passed'''
+import sys
+
+#message = "hello i am working"
+print(message)
+print (sys.argv[0])
+print("hi, "+sys.argv[1])
+
